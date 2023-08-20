d2f7449771b7ab7acae8f155f2c097d6bb74914f
Calvin Figuereo-Supraner <mail@calvin.page>
2023-08-20 13:31:28 -0700

    Use render configs

--- cmd/rt/main.go
+++ cmd/rt/main.go
@@ -1,30 +1,20 @@
 package main
 
 import (
-	"image"
-	"image/color"
 	"image/png"
 	"log"
 	"os"
+
+	"github.com/clfs/m/raytracer"
 )
 
 func main() {
-	log.SetFlags(0)
-
-	var height, width = 256, 256
-
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	for j := 0; j < height; j++ {
-		for i := 0; i < width; i++ {
-			r := float64(i) / float64(width-1)
-			g := float64(j) / float64(height-1)
-			b := 0.0
-			ir := int(255.999 * r)
-			ig := int(255.999 * g)
-			ib := int(255.999 * b)
-			img.SetRGBA(i, j, color.RGBA{uint8(ir), uint8(ig), uint8(ib), 255})
-		}
+	cfg := raytracer.Config{
+		ImageHeight: 256,
+		ImageWidth:  256,
 	}
+
+	img := raytracer.Render(cfg)
 
 	f, err := os.Create("demo.png")
 	if err != nil {
---
+++ raytracer/render.go
@@ -0,0 +1,30 @@
+package raytracer
+
+import (
+	"image"
+	"image/color"
+	"log"
+)
+
+var Logger = log.Default()
+
+type Config struct {
+	ImageHeight int
+	ImageWidth  int
+}
+
+func Render(c Config) image.Image {
+	img := image.NewRGBA(image.Rect(0, 0, c.ImageWidth, c.ImageHeight))
+	for j := 0; j < c.ImageHeight; j++ {
+		for i := 0; i < c.ImageWidth; i++ {
+			r := float64(i) / float64(c.ImageWidth-1)
+			g := float64(j) / float64(c.ImageHeight-1)
+			b := 0.0
+			ir := int(255.999 * r)
+			ig := int(255.999 * g)
+			ib := int(255.999 * b)
+			img.SetRGBA(i, j, color.RGBA{uint8(ir), uint8(ig), uint8(ib), 255})
+		}
+	}
+	return img
+}
