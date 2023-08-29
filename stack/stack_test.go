5a952baaac71bf8b15aca350bdd96979a8915c3c
Calvin Figuereo-Supraner <mail@calvin.page>
2023-08-28 21:14:50 -0700

    Clarify example

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -3,27 +3,28 @@ package stack
 import "fmt"
 
 func ExampleStack() {
-	s := NewStack(1, 2)
+	s := NewStack(7, 8)
 
-	s.Push(3)
+	s.Push(9)
 
 	fmt.Println(s.Count())
+
 	fmt.Println(s.Peek())
-
 	fmt.Println(s.Pop())
 	fmt.Println(s.Pop())
 	fmt.Println(s.Pop())
 
 	fmt.Println(s.Count())
+
 	fmt.Println(s.Peek())
 	fmt.Println(s.Pop())
 
 	// Output:
 	// 3
-	// 3 true
-	// 3 true
-	// 2 true
-	// 1 true
+	// 9 true
+	// 9 true
+	// 8 true
+	// 7 true
 	// 0
 	// 0 false
 	// 0 false
